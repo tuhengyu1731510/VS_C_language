b754d1d2e973e0d1a2187895e53c12df654eb1a3
tuhengyu1731510 <129579263+tuhengyu1731510@users.noreply.github.com>
2023-07-20 21:38:59 +0800

    C语言hello_bit

---
+++ test_7_20_1/test_7_20_1/test.c
@@ -0,0 +1,28 @@
+////#include<stdio.h>
+////int main()//main函数是程序的入口，有且只有一个
+////{			//第一行
+////	printf("hello world!");
+////	return 0;
+////}
+////
+////#include<stdio.h>
+////int main()
+////{
+////	printf("%s\n", "abc");
+////	printf("%s\n", "a");
+////	return 0;
+////}
+//#include <stdio.h>
+//#include <string.h>
+//
+//int main()
+//{
+//    char arr[] = { 'b', 'i', 't' };
+//    printf("%d\n", strlen(arr));
+//    return 0;
+//}
+#include <stdio.h>
+int main()
+{
+	printf("%c", 31);
+}
